d52e6fb0626f35d5192ddf03a2395bfc61daa80f
Parth Goel <parth.179301138@muj.manipal.edu>
2018-03-03 02:01:22 +0530

    login handler

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -1,7 +1,23 @@
-<form action="register.php" method="POST">
-    <input type="email" name="log_email" placeholder="Email Address">
-    <br>
-    <input type="password" name="log_password" placeholder="Password">
-    <br>
-    <input type="submit" name="login_button" value="Login">
-</form>
+<?php
+
+if(isset($_POST['login_button'])){
+
+    $email = filter_var($_POST['log_email'], FILTER_SANITIZE_EMAIL); //sanitize email
+
+    $_SESSION['log_email'] = $email; //store email into sesssion variable
+    $password=md5($POST['log_password']); //get password
+
+    $check_database_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND password='$password'");
+    $check_login_query =mysqli_num_rows($check_database_query);
+
+    if($check_login_query ==1){
+        $row = mysqli_fetch_array($check_database_query);
+        $username = $row['username'];
+
+        $SESSION['username'] = $username;
+        header("Location: index.php");
+        exit();
+    }
+}
+
+?>
