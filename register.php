cbf5da682d883171f9fc1c24a5a0fb5e76178bdf
shreyam <shreyam@github.com>
2018-03-03 01:28:01 +0530

    register code

---
+++ register.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>Welcome to social website</title>
+</head>
+<body>
+    <form action = "register.php" method= "POST">
+      
+        <input type="text" name="reg_fName" placeholder="First name" required>
+        <br>
+        <input type="text" name="reg_lName" placeholder="Last Name" >
+        <br>
+        <input type="email" name="reg_Email" placeholder="Email" required>
+        <br>
+        <input type="email2" name="reg_Email2" placeholder="Confirm Email" required>
+        <br>
+        <input type="password" name="reg_Password" placeholder="Password" required>
+        <br>
+        <input type="password2" name="reg_Password2" placeholder="Confirm Password" required>
+        <br>
+        <input type="submit" name="reg_Button" value="Register">
+    
+    
+    
+    
+    
+    </form>
+</body>
+
+
+
+
+</html>
