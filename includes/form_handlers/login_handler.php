2cac254a7c78787716dd710868b78176279ba325
Parth Goel <parth.179301138@muj.manipal.edu>
2018-03-03 00:46:00 +0530

    login code

---
+++ includes/form_handlers/login_handler.php
@@ -0,0 +1,7 @@
+<form action="register.php" method="POST">
+    <input type="email" name="log_email" placeholder="Email Address">
+    <br>
+    <input type="password" name="log_password" placeholder="Password">
+    <br>
+    <input type="submit" name="login_button" value="Login">
+</form>
