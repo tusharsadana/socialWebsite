33727fe1f4a59d4a93746708e12d626cc342ebd7
Parth Goel <parth.179301138@muj.manipal.edu>
2018-03-17 02:08:27 +0530

    require statement added

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php 
-$con = mysqli_connect("localhost", "root", "", "social");//connection variable
-
-if(mysqli_connect_errno())
- {
-     echo "Failed to connect" . mysqli_connect_errno();
- }
-
-$query = mysqli_query($con,"INSERT INTO test VALUES ('2', 'John Wick')");
+require 'config/config.php';
 
 ?>
 
